898afc35fd4e961c4f37e0861df42bce5b668ae6
Sun Lee <chlee@knox.edu>
2024-11-15 03:57:19 -0600

    neww

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 페이지가 로드될 때 이미지와 텍스트가 서서히 나타나는 효과
+
 document.addEventListener("DOMContentLoaded", () => {
     const profileSection = document.querySelector('.profile-section');
     profileSection.style.opacity = 0;
