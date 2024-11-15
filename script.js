14fea5e7d7ae759126f839fa2d962e4c4a64afba
Sun Lee <chlee@knox.edu>
2024-11-15 03:29:14 -0600

    New

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Welcome to my Profile!');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nameElement = document.getElementById("name");
+    const intro = document.getElementById("intro");
+    const content = document.getElementById("content");
+
+    nameElement.addEventListener("click", () => {
+
+        intro.classList.add("hidden");
+
+
+        setTimeout(() => {
+            intro.style.display = "none";
+            content.style.display = "block";
+            document.body.style.overflow = "auto"; 
+        }, 1000);
+    });
+});
